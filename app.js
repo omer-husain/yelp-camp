88c43ef421a589db4b7d033ff3aa8cb9f4a2aee7
Omer Husain <husain.omer@gmail.com>
2021-12-27 12:33:39 -0500

    added two routes, for form submission of new record and where form submits to - create record

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ db.once("open", () => {
 
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
@@ -27,9 +29,20 @@ app.get("/campgrounds", async (req, res) => {
   const campgrounds = await Campground.find({});
   res.render("campgrounds/index", { campgrounds });
 });
+
+app.get("/campgrounds/new", (req, res) => {
+  res.render("campgrounds/new");
+});
+
 app.get("/campgrounds/:id", async (req, res) => {
   const campground = await Campground.findById(req.params.id);
   res.render("campgrounds/show", { campground });
+});
+
+app.post("/campgrounds", async (req, res) => {
+  const campground = new Campground(req.body.campground);
+  await campground.save();
+  res.redirect(`/campgrounds/${campground._id}`);
 });
 
 app.listen(3000, () => {
