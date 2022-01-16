c80cf71bda69762c2a502f3123912611cd6d2bb8
Omer Husain <husain.omer@gmail.com>
2022-01-15 22:35:35 -0500

    added image upload and delete functionality

--- seeds/index.js
+++ seeds/index.js
@@ -30,10 +30,19 @@ const seedDB = async () => {
       author: "61d8a0dc326dedb93161dfb3",
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
-      image: "https://source.unsplash.com/collection/483251",
       description:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse auctor eu lacus non iaculis. Mauris elit dolor, luctus a diam sed, fermentum rutrum mauris. Sed sit amet leo ut ante pretium euismod id sit amet lorem. Phasellus ac magna id nisi imperdiet malesuada. Praesent erat ligula, dapibus sed mauris nec, tristique eleifend eros. Integer consequat dolor in dignissim pharetra. Vestibulum lacus massa, aliquet id est et, pretium tempus lectus.",
       price: price,
+      images: [
+        {
+          url: "https://res.cloudinary.com/dkpn2dtyr/image/upload/v1642124892/YelpCamp/fkt0qdfapzj5uwqhb3v4.jpg",
+          filename: "YelpCamp/fkt0qdfapzj5uwqhb3v4",
+        },
+        {
+          url: "https://res.cloudinary.com/dkpn2dtyr/image/upload/v1642124892/YelpCamp/pqdr4atreiyujhozpoxo.jpg",
+          filename: "YelpCamp/pqdr4atreiyujhozpoxo",
+        },
+      ],
     });
 
     await camp.save();
